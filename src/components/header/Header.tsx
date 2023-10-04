8b762a51d93c5f01430da803470efb351f9c0250
Ameen <aladeameen@gmail.com>
2023-10-04 21:06:30 +0100

    fix module not found issue on vercel

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,45 @@
+import styles from "./header.module.scss";
+
+const Header = (): JSX.Element => {
+  return (
+    <>
+      <header id={styles.header}>
+        <div className={styles.wrapperLanding}>
+          <div className={styles.text}>
+            <div className={styles.textI}>
+              <div className={styles.gradientText}>
+                <h1>Software</h1>
+
+                <h1>
+                  Developer<span>.</span>
+                </h1>
+              </div>
+
+              <p>
+                I find fulfillment in seamlessly blending code and design to
+                craft resilient software that brings joy to users.
+              </p>
+            </div>
+
+            <div className={styles.paraG}>
+              <p id="h1BG">
+                Highly skilled at developing scalable web applications using
+                modern frameworks, with a proven track record of optimizing
+                performance and ensuring robust security.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.imageH}>
+            <div>
+              <span className={styles.dp}></span>
+            </div>
+            <span></span>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
