d2c38934b1d3c715138e4a958f98dcf544db920d
Ameen <aladeameen@gmail.com>
2023-10-04 21:18:49 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Layout from "../layouts/layout";
-import Header from "../components/header/Header";
-import About from "../components/about/About";
-import Caption from "../components/caption/Caption";
-import Experience from "../components/experience/Experience";
-import Projects from "../components/projects/Projects";
-import Contact from "../components/contact/Contact";
+import Header from "@/components/header/Header.tsx";
+import About from "@/components/about/About";
+import Caption from "@/components/caption/Caption";
+import Experience from "@/components/experience/Experience";
+import Projects from "@/components/projects/Projects";
+import Contact from "@/components/contact/Contact";
 
 import "@/styles/index.scss";
 
