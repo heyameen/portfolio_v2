4493b7d53ee8b6c577e25a07f91f186c5ab2f3fa
Ameen <aladeameen@gmail.com>
2023-10-04 21:37:53 +0100

    fix type issues

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -2,7 +2,7 @@ import { Fragment, FC } from "react";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 
-export default function Layout(props: any): FC {
+export default function Layout(props: any) {
   const { children, ...customMeta } = props;
 
   return (
