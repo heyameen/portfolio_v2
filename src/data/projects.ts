9f1983ab42163a9b204236e10d6e4746ce8eefb9
Ameen <aladeameen@gmail.com>
2023-10-07 09:17:32 +0100

    add project section to the page

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+export const projectData = [
+  {
+    name: "Tunez",
+    desc: "A web music player that allows users to play and manage their music playlist seamlessly.",
+    lang: "NextJS + PostgreSQL + Tailwind",
+    link: "https://tunez.vercel.app/",
+  },
+  {
+    name: "Space",
+    desc: "Space is a convenient hotel booking landing page",
+    lang: "HTML + CSS + JavaScript",
+    link: "https://circlefinance.ng",
+  },
+  {
+    name: "Todo",
+    desc: "Feature-rich productivity application that helps users keep track of their tasks efficiently.",
+    lang: "REACT + REDUX + SCSS",
+    link: "https://todo-nine-tau.vercel.app/",
+  },
+];
