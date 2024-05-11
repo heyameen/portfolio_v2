a482e82a21359082850107f8711b57f2ccb06dc8
ameen <ameen@invisiblehomes.co.uk>
2024-05-11 21:41:30 +0100

    change stack implementation

--- src/components/stack/Stack.tsx
+++ src/components/stack/Stack.tsx
@@ -3,59 +3,29 @@ import React from 'react'
 import styles from './stack.module.scss'
 import ICONS from '@/data/icons'
 
-const BLURS = 3
-
-const Stack = (): React.JSX.Element => {
-  return (
-    <div
-      className={styles.resizable}
-      data-translate="items"
-      data-direction="horizontal"
-      data-blurring={true}
-      data-outline={false}
-      data-play-state="running"
-      data-spill={false}
-      style={{
-        '--speed': 25,
-        '--count': 12,
-        '--scale': 1,
-        '--blur': 1.8,
-        '--blurs': BLURS,
-      }}
-    >
-      <div className={styles.container}>
-        <div className={`${styles.blur} ${styles.blurLeft}`}>
-          {new Array(BLURS).fill(0).map((_, index) => (
-            <div key={index} style={{ '--index': index }} />
-          ))}
+const Stack: React.FC = () => {
+    const iconCount = typeof window !== 'undefined' ? Math.ceil(window.innerWidth / 100) + 1 : 0;
+    return (
+        <div className={styles.container}>
+            <div className={`${styles.blur} ${styles.blurLeft}`} />
+            <ul className={styles.list}>
+                {[...Array(iconCount)].flatMap(() =>
+                    ICONS.map((icon, index) => (
+                        <li key={`${index}`} className={styles.item}>
+                            <div dangerouslySetInnerHTML={{__html: icon}}/>
+                        </li>
+                    ))
+                )}
+            </ul>
+            <div className={`${styles.blur} ${styles.blurRight}`}/>
         </div>
-        <ul className={styles.ul}>
-          {new Array(12).fill(0).map((item, index) => {
-            return (
-              <li
-                className={styles.li}
-                key={`index-${Date.now()}--${index}`}
-                style={{ '--index': index }}
-                dangerouslySetInnerHTML={{
-                  __html: ICONS[index % ICONS.length],
-                }}
-              />
-            )
-          })}
-        </ul>
-        <div className={`${styles.blur} ${styles.blurRight}`}>
-          {new Array(BLURS).fill(0).map((_, index) => (
-            <div
-              key={index}
-              style={{
-                '--index': index + 1,
-              }}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Stack
+export default Stack;
+
+
+
+
+
+// Stack
